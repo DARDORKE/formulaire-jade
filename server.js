9ba2f165322fdc6a5da4538b1c3215d20de1f506
Kévy <kevy.dardor@gmail.com>
2024-01-17 10:02:06 +0100

    Setup build heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/formulaire-jade'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/formulaire-jade.index.html'));
+  res.sendFile(path.join(__dirname+'/dist/formulaire-jade/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
