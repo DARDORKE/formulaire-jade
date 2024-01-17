77472b899b957223eae0b2d90cbcf822bc9376ba
Kévy <kevy.dardor@gmail.com>
2024-01-17 23:00:17 +0100

    Animation + retour

--- src/app/quizz/quiz.service.ts
+++ src/app/quizz/quiz.service.ts
@@ -4,23 +4,40 @@ import {Injectable} from "@angular/core";
   providedIn: 'root'
 })
 export class QuizService {
+  private history: { questionId: number, selectedOption: any }[] = [];
   private questions = [
     {
       id: 1,
       questionText: "Question 1 : De quelle couleur est la mer ?",
       options: [
         {answerText: "Réponse A1", nextQuestionId: 2},
-        {answerText: "Réponse A2", nextQuestionId: 3},]
+        {answerText: "Réponse A2", nextQuestionId: 2},]
     },
     {
       id: 2,
       questionText: "Question 2 : Comment sont vont les vaches ?",
       options: [
-        {answerText: "Réponse B1", nextQuestionId: 4},
-        {answerText: "Réponse B2", nextQuestionId: 5},]
+        {answerText: "Réponse B1", nextQuestionId: 1},
+        {answerText: "Réponse B2", nextQuestionId: 1},]
     },
     // autres questions...
   ];
+
+  addResponse(questionId: number, selectedOption: any) {
+    this.history.push({ questionId, selectedOption });
+  }
+
+  getPreviousQuestion() {
+    if (this.history.length > 1) {
+      this.history.pop(); // Enlève la question actuelle
+      return this.history[this.history.length - 1]; // Retourne la question précédente
+    }
+    return null;
+  }
+
+  getHistory() {
+    return this.history;
+  }
 
   getQuestion(id: number) {
     return this.questions.find(question => question.id === id);
--- src/app/quizz/quizz.component.ts
+++ src/app/quizz/quizz.component.ts
@@ -3,6 +3,8 @@ import {QuizService} from "./quiz.service";
 import {NgForOf, NgIf} from "@angular/common";
 import {RadioButtonModule} from "primeng/radiobutton";
 import {FormsModule} from "@angular/forms";
+import {ButtonModule} from "primeng/button";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 @Component({
   selector: 'app-quizz',
   standalone: true,
@@ -10,15 +12,32 @@ import {FormsModule} from "@angular/forms";
     NgIf,
     NgForOf,
     RadioButtonModule,
-    FormsModule
+    FormsModule,
+    ButtonModule
   ],
   templateUrl: './quizz.component.html',
-  styleUrl: './quizz.component.scss'
+  styleUrl: './quizz.component.scss',
+  animations: [
+    trigger('questionAnimation', [
+      state('enter', style({ opacity: 1, transform: 'translateX(0)' })),
+      state('leave', style({ opacity: 0, transform: 'translateX(-100%)' })),
+      transition('* => enter', [
+        style({ opacity: 0, transform: 'translateX(100%)' }),
+        animate('0.5s ease-out')
+      ]),
+      transition('* => leave', [
+        animate('0.5s ease-in', style({ opacity: 0, transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
+
 })
 export class QuizzComponent implements OnInit{
   currentQuestionId = 1;
   currentQuestion: any;
   selectedOption: any;  // Ajouté pour gérer l'option sélectionnée
+  questionHistory: { questionId: number, selectedOption: any }[] = [];
+  currentQuestionVisible = true;
 
   constructor(private quizService: QuizService) {}
 
@@ -27,15 +46,37 @@ export class QuizzComponent implements OnInit{
   }
 
   loadQuestion(id: number) {
-    this.currentQuestion = this.quizService.getQuestion(id);
-    this.selectedOption = null; // Réinitialiser l'option sélectionnée
+    this.currentQuestionVisible = false; // Cachez la question actuelle
+
+    setTimeout(() => {
+      this.currentQuestion = this.quizService.getQuestion(id);
+      this.selectedOption = null; // Réinitialiser l'option sélectionnée
+      this.currentQuestionVisible = true; // Affichez la nouvelle question
+    }, 500); // Le délai devrait correspondre à la durée de l'animation
   }
 
   onSelectOption() {
     if (this.selectedOption) {
-      // Ici, gérer la logique pour passer à la question suivante en fonction de l'option sélectionnée
+      this.questionHistory.push({ questionId: this.currentQuestionId, selectedOption: this.selectedOption });
+
+      console.log(this.questionHistory);
       this.currentQuestionId = this.selectedOption.nextQuestionId;
       this.loadQuestion(this.currentQuestionId);
     }
+  }
+
+  goToPreviousQuestion() {
+    if (this.questionHistory.length > 0) {
+      const previous = this.questionHistory.pop(); // Retirer et obtenir la question précédente
+      console.log(this.questionHistory);
+      this.currentQuestionId = previous ? previous.questionId : 0;
+      this.selectedOption = previous?.selectedOption;
+      this.loadQuestion(this.currentQuestionId);
+    }
+  }
+
+  calculateFinalOutcome() {
+    // Implémentez votre logique ici pour analyser les réponses
+    // et fournir une conclusion ou une évaluation basée sur les choix de l'utilisateur.
   }
 }
